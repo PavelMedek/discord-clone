3aa6d522ec298c70ce21cd20861336b28dc01a45
PavelMedek <medek41@gmail.com>
2023-10-05 13:48:28 +0200

    UploadThing setup

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,6 +24,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import FileUpload from "../file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -75,7 +76,7 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                {/* <FormField
+                <FormField
                   control={form.control}
                   name="imageUrl"
                   render={({ field }) => (
@@ -89,7 +90,7 @@ const InitialModal = () => {
                       </FormControl>
                     </FormItem>
                   )}
-                /> */}
+                />
               </div>
 
               <FormField
